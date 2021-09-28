56b2c965f8cececb6850514334c749813a5d61e4
Mike Mitchell <mikemitchell574@gmail.com>
2021-09-28 16:48:36 -0400

    Replace image with countdown timer

---
+++ script.js
@@ -0,0 +1,18 @@
+let countDownDate = new Date("Oct 14, 2021 15:37:25").getTime();
+
+let x = setInterval(function () {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+      }
+}, 1000)
